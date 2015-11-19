6f7f5faea2fc700d339a9c34ff424135387f6adc
wmvsilva <silva.w@husky.neu.edu>
2015-11-19 16:18:00 -0500

    Added all relationships to schema

--- database/schema.sql
+++ database/schema.sql
@@ -36,6 +36,7 @@ CREATE TABLE album
 DROP TABLE IF EXISTS `general_song`;
 CREATE TABLE general_song
 (
+	song_id INT NOT NULL AUTO_INCREMENT,
 	track_name VARCHAR(70) NOT NULL,
     lyrics TEXT,
     audio_sample VARCHAR(512),
@@ -43,8 +44,12 @@ CREATE TABLE general_song
     album_id INT NOT NULL,
     
     CONSTRAINT
-		pk_general_song_album_id_track_name
-    PRIMARY KEY ( album_id, track_name ),
+		pk_general_song_song_id
+    PRIMARY KEY ( song_id ),
+    
+	CONSTRAINT
+		uk_general_song_track_name_album_id
+	UNIQUE KEY ( track_name, album_id ),
     
     CONSTRAINT
 		fk_general_song_album_album_id
@@ -58,19 +63,18 @@ CREATE TABLE general_song
 DROP TABLE IF EXISTS `single_song`;
 CREATE TABLE single_song
 (
-	track_name VARCHAR(70) NOT NULL,
-    album_id INT NOT NULL,
+	song_id INT NOT NULL,
     release_date DATE,
     cover_art VARCHAR(512),
     
     CONSTRAINT
-		pk_single_song_track_name_album_id
-	PRIMARY KEY ( track_name, album_id ),
+		pk_single_song_song_id
+	PRIMARY KEY ( song_id ),
     
     CONSTRAINT
 		fk_single_song_general_song_track_name_album_id
-	FOREIGN KEY ( album_id, track_name )
-		REFERENCES general_song( album_id, track_name )
+	FOREIGN KEY ( song_id )
+		REFERENCES general_song( song_id )
         ON DELETE CASCADE
         ON UPDATE CASCADE
         
@@ -79,18 +83,17 @@ CREATE TABLE single_song
 DROP TABLE IF EXISTS `album_song`;
 CREATE TABLE album_song
 (
-	track_name VARCHAR(70) NOT NULL,
-    album_id INT NOT NULL,
+	song_id INT NOT NULL,
     track_number TINYINT UNSIGNED,
     
     CONSTRAINT
-		pk_album_song_track_name_album_id
-	PRIMARY KEY ( track_name, album_id ),
+		pk_album_song_song_id
+	PRIMARY KEY ( song_id ),
     
     CONSTRAINT
-		fk_album_song_general_song_track_name_album_id
-	FOREIGN KEY ( album_id, track_name )
-		REFERENCES general_song( album_id, track_name )
+		fk_album_song_general_song_song_id
+	FOREIGN KEY ( song_id )
+		REFERENCES general_song( song_id )
         ON DELETE CASCADE
         ON UPDATE CASCADE
         
@@ -147,19 +150,18 @@ CREATE TABLE store
 DROP TABLE IF EXISTS `artists_of_songs`;
 CREATE TABLE artists_of_songs
 (
-	album_id INT NOT NULL,
-    track_name VARCHAR(70) NOT NULL,
+	song_id INT NOT NULL,
     artist_name VARCHAR(70),
     
     
     CONSTRAINT
-		pk_album_id_track_name_artist_name
-    PRIMARY KEY ( album_id, track_name, artist_name ),
+		pk_song_id_artist_name
+    PRIMARY KEY ( song_id, artist_name ),
     
 	CONSTRAINT
-		fk_artists_of_songs_song_album_id_track_name
-	FOREIGN KEY ( album_id, track_name )
-		REFERENCES general_song( album_id, track_name )
+		fk_artists_of_songs_general_song
+	FOREIGN KEY ( song_id )
+		REFERENCES general_song( song_id )
         ON DELETE CASCADE
         ON UPDATE CASCADE,
         
@@ -172,13 +174,167 @@ CREATE TABLE artists_of_songs
     
 ) ENGINE = INNODB;
 
-CREATE TRIGGER tg_artists_of_songs_total_song_participation
-BEFORE INSERT ON general_song
-FOR EACH ROW
-  SET NEW.track_name = IF(
-    (
-      SELECT COUNT(*) FROM artists_of_songs WHERE track_name = NEW.track_name AND album_id = NEW.album_id
-    ) != 0,
-    NEW.track_name,
-    NULL
-  );
+DROP TABLE IF EXISTS `featured_artists_of_songs`;
+CREATE TABLE featured_artists_of_songs
+(
+	song_id INT NOT NULL,
+    featured_artist_name VARCHAR(70) NOT NULL,
+    
+    
+    CONSTRAINT
+		pk_song_id_artist_name
+    PRIMARY KEY ( song_id, featured_artist_name ),
+    
+	CONSTRAINT
+		fk_featured_artists_of_songs_general_song
+	FOREIGN KEY ( song_id )
+		REFERENCES general_song( song_id )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+      
+	CONSTRAINT
+		fk_featured_artists_of_songs_artist_artist_name
+	FOREIGN KEY ( featured_artist_name )
+		REFERENCES artist( artist_name )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    
+) ENGINE = INNODB;
+
+DROP TABLE IF EXISTS `genre_of_song`;
+CREATE TABLE genre_of_song
+(
+	song_id INT NOT NULL,
+    genre_name VARCHAR(70) NOT NULL,
+    
+    
+    CONSTRAINT
+		pk_song_id_genre_name
+    PRIMARY KEY ( song_id, genre_name ),
+    
+	CONSTRAINT
+		fk_genre_of_song_general_song
+	FOREIGN KEY ( song_id )
+		REFERENCES general_song( song_id )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+        
+	CONSTRAINT
+		fk_genre_of_song_genre_genre_name
+	FOREIGN KEY ( genre_name )
+		REFERENCES genre( genre_name )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    
+) ENGINE = INNODB;
+
+DROP TABLE IF EXISTS `artists_of_albums`;
+CREATE TABLE artists_of_albums
+(
+	album_id INT NOT NULL,
+    artist_name VARCHAR(70),
+    
+    
+    CONSTRAINT
+		pk_album_id_artist_name
+    PRIMARY KEY ( album_id, artist_name ),
+    
+	CONSTRAINT
+		fk_artists_of_albums_album_album_id
+	FOREIGN KEY ( album_id )
+		REFERENCES album( album_id )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+        
+	CONSTRAINT
+		fk_artists_of_albumss_artist_artist_name
+	FOREIGN KEY ( artist_name )
+		REFERENCES artist( artist_name )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    
+) ENGINE = INNODB;
+
+DROP TABLE IF EXISTS `genre_of_album`;
+CREATE TABLE genre_of_album
+(
+	album_id INT NOT NULL,
+    genre_name VARCHAR(70) NOT NULL,
+    
+    
+    CONSTRAINT
+		pk_album_id_genre_name
+    PRIMARY KEY ( album_id, genre_name ),
+    
+	CONSTRAINT
+		fk_genre_of_album_album_id
+	FOREIGN KEY ( album_id )
+		REFERENCES album( album_id )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+        
+	CONSTRAINT
+		fk_genre_of_album_genre_genre_name
+	FOREIGN KEY ( genre_name )
+		REFERENCES genre( genre_name )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    
+) ENGINE = INNODB;
+
+DROP TABLE IF EXISTS `format_of_album`;
+CREATE TABLE format_of_album
+(
+	distribution_id INT NOT NULL AUTO_INCREMENT,
+	album_id INT NOT NULL,
+    format_name VARCHAR(70) NOT NULL,
+    
+    CONSTRAINT
+		pk_format_of_album_distribution_id
+    PRIMARY KEY ( distribution_id ),
+    
+	CONSTRAINT
+		uk_format_of_album_album_id_format_name
+	UNIQUE KEY ( album_id, format_name ),
+    
+	CONSTRAINT
+		fk_format_of_album_album_id
+	FOREIGN KEY ( album_id )
+		REFERENCES album( album_id )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+        
+	CONSTRAINT
+		fk_format_of_album_format_format_name
+	FOREIGN KEY ( format_name )
+		REFERENCES music_format( format_name )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    
+) ENGINE = INNODB;
+
+DROP TABLE IF EXISTS `store_catalog`;
+CREATE TABLE store_catalog
+(
+	store_name VARCHAR(70) NOT NULL,
+	distribution_id INT NOT NULL,
+    
+    CONSTRAINT
+		pk_store_catalog_store_name_distribution_id
+    PRIMARY KEY ( store_name, distribution_id ),
+    
+	CONSTRAINT
+		fk_store_catalog_store_store_name
+	FOREIGN KEY ( store_name )
+		REFERENCES store( store_name )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+        
+	CONSTRAINT
+		fk_store_catalog_format_of_album_distribution_id
+	FOREIGN KEY ( distribution_id )
+		REFERENCES format_of_album( distribution_id )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    
+) ENGINE = INNODB;
